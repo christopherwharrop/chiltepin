88ba0635b26c57df2a7c26ca0e6c213c96227b0a
Christopher Harrop <christopher.w.harrop@noaa.gov>
2015-12-01 17:17:58 +0000

    Add use of Scala Native Intervace for calling the standard C library to obtain process uid info.

--- build.sbt
+++ build.sbt
@@ -2,11 +2,13 @@ lazy val commonSettings = Seq(
   version := "1.0",
   scalaVersion := "2.10.4",
   scalacOptions ++= Seq("-feature", "-deprecation"),
+//  javacOptions ++= Seq("-Xlint:unchecked"),
   resolvers += "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/",
   libraryDependencies ++= Seq(  
     "com.typesafe.akka" %% "akka-actor" % "2.3.11",
     "com.typesafe.akka" %% "akka-remote" % "2.3.11",
     "com.typesafe.akka" %% "akka-persistence-experimental" % "2.3.11",
+    "net.java.dev.jna" % "jna-platform" % "4.0.0",
     "joda-time" % "joda-time" % "2.4",
     "org.joda" % "joda-convert" % "1.6",
     "com.typesafe.slick" %% "slick" % "2.0.0",
--- chiltepin/src/main/scala/chiltepin.scala
+++ chiltepin/src/main/scala/chiltepin.scala
@@ -4,9 +4,35 @@ import com.typesafe.config.ConfigRenderOptions
 import java.io.File
 import java.io.PrintWriter
 
+import com.sun.jna.Structure
+import sna.Library
+
 object Chiltepin extends RunCommand {
 
   def main(args: Array[String]) {
+
+
+  val cLib = Library("c")  // Note-1
+
+  val uid = cLib.geteuid()[Int]  // Note-2
+  cLib.printf("geteuid: %d\n", uid)[Int]  // Note-3
+
+
+  class passwd extends Structure {
+    var pw_name: String = _   // username
+    var pw_passwd: String = _ // user password
+    var pw_uid: Int = _       // user ID
+    var pw_gid: Int = _       // group ID
+    var pw_gecos: String = _  // real name
+    var pw_dir: String = _    // home directory
+    var pw_shell: String = _  // shell program 
+  }
+
+  val passwdent = cLib.getpwuid(uid)[passwd]  // Note-3
+  println(passwdent.pw_name)
+  println(passwdent.pw_gid)
+  println(passwdent.pw_dir)
+
 
     // Get default configuration for the chiltepin
     val defaultConfig = ConfigFactory.load()
