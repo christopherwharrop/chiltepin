8fd31936e39ca9360fe43de32f3f07ec915df024
Christopher Harrop <christopher.w.harrop@noaa.gov>
2015-12-01 12:22:12 -0700

    Adding source file that was forgotten in last commit

---
+++ src/main/scala/whoami.scala
@@ -0,0 +1,32 @@
+import com.sun.jna.Structure
+import sna.Library
+
+// Create the structure returned by the call to getpwuid()
+class Passwd extends Structure {
+  var pw_name: String = _   // username
+  var pw_passwd: String = _ // user password
+  var pw_uid: Int = _       // user ID
+  var pw_gid: Int = _       // group ID
+  var pw_gecos: String = _  // real name
+  var pw_dir: String = _    // home directory
+  var pw_shell: String = _  // shell program
+}
+
+case class WhoAmIResult(username: String, uid: Int, gid: Int, home: String)
+
+trait WhoAmI {
+
+  // Get a handle to the C library
+  val cLib = Library("c") 
+
+  def whoAmI(): WhoAmIResult = {
+    // Call geteuid() in C library to get the effective UID
+    val uid = cLib.geteuid()[Int]
+
+    // Call getpwuid to look up password entry for the UID
+    val passwdent = cLib.getpwuid(uid)[Passwd]
+
+    WhoAmIResult(passwdent.pw_name, passwdent.pw_uid, passwdent.pw_gid, passwdent.pw_dir)
+  }   
+
+}
